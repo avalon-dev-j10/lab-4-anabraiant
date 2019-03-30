6c316f400d98b1ab63829eaa8bba8a31f564914b
anabraiant <48486410+anabraiant@users.noreply.github.com>
2019-03-30 21:29:12 +0300

    <tmp>  нужно пояснение по классу от Comparator
    нужна помощь по class ComparatorClass, нужно ли его делать обстакным или же оверрайдить метод  compare() через класс SortClass. Нет понимания как реализовать данный класс

---
+++ src/ru/avalon/java/dev/j10/labs/SortClass.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.avalon.java.dev.j10.labs;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author admin
+ */
+public class SortClass implements Sort{
+    
+    
+
+    // Выполняет сортировку переданного массива объектов @param array массив, подлежащий сортировке
+    @Override
+    public void sort(Object[] array) { 
+        if (array instanceof Comparable[]){ // проверяем не наслед. ли Comparable
+            sort((Comparable[]) array); //если да, то сортируем с compareTo
+        }
+       }
+    
+    // Выполняет сортировку массива сравниваемых объектов. 
+    @Override
+    public void sort(Comparable[] array) {
+            for (int i = 0; i < array.length-1; i++) {
+                for (int j = 0; j < array.length -1 ; j++){ 
+                    if ((array[j].compareTo(array[j+1])) == 1){//проверка объекта, если он больше, чем следущий, то меняем местами 
+                        Comparable tmp = array[j+1];
+                        array[j+1] = array[j];
+                        array[j] = tmp;
+                }
+                            
+                }
+    
+            }
+         }
+
+    // Выполняет сортировку массива объектов c использованием переданного объекта типа Comparator.
+    @Override
+    public void sort(Object[] array, Comparator comparator) { //реализация компоратора такая же как Comparable
+        for (int i =0; i < array.length; i++){
+            for (int j = 0; j < array.length; j++){
+                if( comparator.compare(array[j], array[j+1]) == -1 ){ ////проверка объекта, если он меньше, чем следущий, то меняем местами
+                    Object tmp = array[i+1];
+                    array[i+1]= array[i];
+                    array[i]= tmp;
+                }
+            }
+        }
+         }
+    
+}
